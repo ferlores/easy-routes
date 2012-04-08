e2edbb84a718c67a4fbca104b9930dc8438b893b
Fernando Lores <ferlores@gmail.com>
2012-04-08 19:57:53 -0300

    Add LICENSE and README

--- testing/app.js
+++ testing/app.js
@@ -34,7 +34,7 @@ app.listen(3000, function(){
     console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
 	// TESTING
-	//require('./tests')(app);
+	require('./tests')(app);
 });
 
 
