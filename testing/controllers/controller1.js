2848d4b2d91f55f09f328d534e7460bc69b6cffe
Fernando Lores <ferlores@gmail.com>
2012-04-14 11:44:45 -0300

    Support for before_filter and after_filter

--- testing/controllers/controller1.js
+++ testing/controllers/controller1.js
@@ -20,6 +20,10 @@ function printRoute (req, res){
 
 //Public
 module.exports = {
+	before_filter: function(req, res){
+		console.log('Action is going to be excecute');
+		return true;
+	},
 	index_GET: function(req, res) {
 		printRoute(req, res);
 	},
@@ -36,6 +40,11 @@ module.exports = {
 	admin_index_PUT: function(req,res){
 		res.write(req.body.username);
 		printRoute(req, res);	
+	},
+	after_filter: function(req, res){
+		//this will be excecuted inmediatly after an action ends
+		//doesnt support async calls
+		console.log('Action Excecuted!');
 	}	
 }
 
